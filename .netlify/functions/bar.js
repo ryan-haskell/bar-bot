998349713fbcce6a5319b94b3bcfbba693c14338
Ryan Haskell-Glatz <rhaskellglatz@onenorth.com>
2019-03-07 22:19:15 -0600

    eh

--- .netlify/functions/bar.js
+++ .netlify/functions/bar.js
@@ -1,20 +1,32 @@
+import axios from 'axios'
+const bars = [
+  'Shamrock Club',
+  'Monk\'s',
+  'Snickers',
+  'Rossi\'s',
+  'Green St. Local',
+  'Public House',
+  'Dugans',
+  'Wise Owl',
+  'Garbage Bar',
+  'Richard\'s',
+  'Ceres',
+  'Boss Bar'
+]
+
 exports.handler = function (event, context, callback) {
-  const bars = [
-    'Shamrock Club',
-    'Monk\'s',
-    'Snickers',
-    'Rossi\'s',
-    'Green St. Local',
-    'Public House',
-    'Dugans',
-    'Wise Owl',
-    'Garbage Bar',
-    'Richard\'s',
-    'Ceres',
-    'Boss Bar'
-  ]
+  const bar = bars[parseInt(Math.random() * bars.length)]
+  try {
+    const payload = JSON.parse(event.body || '{}')
+    if (payload.event.type === "app_mention") {
+      console.log('Get mentioned, bro.')
+    }
+  } catch (e) {
+    console.log('Something ain\'t rite.', e)
+  }
+
   callback(null, {
     statusCode: 200,
-    body: bars[parseInt(Math.random() * bars.length)]
+    body: bar
   })
 }
